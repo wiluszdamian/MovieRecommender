4c2631a76046f5c7a72f5fd564bfe50d249dc2de
Damian Wilusz <damians.wilsz@gmail.com>
2023-02-05 12:56:07 +0100

    refactor

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -10,9 +10,10 @@ class MovieController extends Controller
     public function index()
     {
         $client = new Client();
+        $apiKey = env('API_TMDB_KEY');
         
         // Get the 5 most popular movies
-        $popularMoviesResponse = $client->get('https://api.themoviedb.org/3/movie/popular?api_key=32193d189ceb2101de9ed98317acd250&language=en-US&page=1');
+        $popularMoviesResponse = $client->get("https://api.themoviedb.org/3/movie/popular?api_key={$apiKey}&language=en-US&page=1");
         $popularMovies = json_decode($popularMoviesResponse->getBody(), true)['results'];
         $popularMovies = array_slice($popularMovies, 0, 5);
                
@@ -24,13 +25,14 @@ class MovieController extends Controller
     public function show($id)
     {
         $client = new Client();
+        $apiKey = env('API_TMDB_KEY');
 
         // Get information about the movie with the given ID
-        $movieResponse = $client->get("https://api.themoviedb.org/3/movie/{$id}?api_key=32193d189ceb2101de9ed98317acd250&language=en-US");
+        $movieResponse = $client->get("https://api.themoviedb.org/3/movie/{$id}?api_key={$apiKey}&language=en-US");
         $movie = json_decode($movieResponse->getBody(), true);
 
         // Get actors information in the movie with the given ID
-        $actorResponse = $client->get("https://api.themoviedb.org/3/movie/{$id}/credits?api_key=32193d189ceb2101de9ed98317acd250&language=en-US");
+        $actorResponse = $client->get("https://api.themoviedb.org/3/movie/{$id}/credits?api_key={$apiKey}&language=en-US");
         $actors = json_decode($actorResponse->getBody(), true)['cast'];
         $actors = array_slice($actors, 0, 10);
 
