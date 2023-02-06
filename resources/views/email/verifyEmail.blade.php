b8d86ccd4e09b18e9a6220f0d57709eb28252bc8
wiluszdamian <damians.wilusz@gmail.com>
2023-02-06 19:37:07 +0100

    refactor

--- resources/views/email/verifyEmail.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>Email Verification Mail</h1>
-  
-Please verify your email with bellow link: 
-<a href="{{ route('user.verify', $token) }}">Verify Email</a>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API;
 use App\Http\Controllers\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,23 +15,5 @@ use App\Http\Controllers\Auth;
 |
 */
 
-/*
-|--------------------------------------------------------------------------
-| AUTH
-|--------------------------------------------------------------------------
-*/
-Route::get('login', [Auth\AuthController::class, 'index'])->name('login');
-Route::post('post-login', [Auth\AuthController::class, 'postLogin'])->name('login.post'); 
-Route::get('register', [Auth\AuthController::class, 'registration'])->name('register');
-Route::post('post-registration', [Auth\AuthController::class, 'postRegistration'])->name('register.post'); 
-Route::get('logout', [Auth\AuthController::class, 'logout'])->name('logout');
-Route::get('dashboard', [Auth\AuthController::class, 'dashboard'])->middleware(['auth', 'verify_email']); 
-Route::get('account/verify/{token}', [Auth\AuthController::class, 'verifyAccount'])->name('user.verify'); 
-
-/*
-|--------------------------------------------------------------------------
-| BASE
-|--------------------------------------------------------------------------
-*/
 Route::get('/', [API\MovieController::class, 'index']);
 Route::get('/movies/{id}', [API\MovieController::class, 'show']);
