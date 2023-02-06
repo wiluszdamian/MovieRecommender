8a4ad9f8db223bd4e692ec21b0b94c0b9bae7add
wiluszdamian <damians.wilusz@gmail.com>
2023-02-06 19:14:54 +0100

    langs add

---
+++ lang/en/message.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'most_popular_movies' => 'Most Popular Movies',
+    'release_date' => 'Release Date: ',
+    'vote' => 'Vote: ',
+    'description' => 'Description: ',
+    'read_more' => 'Read More',
+    'open_main_menu' => 'Open Main Menu',
+    'home' => 'Home',
+    'movies' => 'Movies',
+    'tv_series' => 'TV Series',
+    'actres' => 'Actres',
+    'sign_in' => 'Sign In',
+    'view_notification' => 'View notification',
+    'open_user_menu' => 'Open user menu',
+    'your_profile' => 'Your Profile',
+    'settings' => 'Settings',
+    'sign_out' => 'Sign out'
+];
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -406,7 +406,7 @@
                     <button type="button"
                         class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                         aria-controls="mobile-menu" aria-expanded="false">
-                        <span class="sr-only">Open main menu</span>
+                        <span class="sr-only">{{ __('message.open_main_menu') }}</span>
                         <!--
                   Icon when menu is closed.
       
@@ -445,16 +445,16 @@
                         <div class="flex space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                             <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                                aria-current="page">Home</a>
+                                aria-current="page">{{ __('message.home') }}</a>
 
                             <a href="/movies"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white rounded-md ">Movies</a>
+                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white rounded-md ">{{ __('message.movies') }}</a>
 
                             <a href="/tv-series"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">TV Series</a>
+                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">{{ __('message.tv_series') }}</a>
 
                             <a href="/actres"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Actres</a>
+                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">{{ __('message.actres') }}</a>
                         </div>
                     </div>
                 </div>
@@ -462,7 +462,7 @@
                     <div>
                         <button type="submit"
                             class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                            <a href="/login">Sign in</a>
+                            <a href="/login">{{ __('message.sign_in') }}</a>
                         </button>
                     </div>
                 @endguest
@@ -470,7 +470,7 @@
                     <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                     <button type="button"
                         class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span class="sr-only">View notifications</span>
+                        <span class="sr-only">{{ __('message.view_notification') }}</span>
                         <!-- Heroicon name: outline/bell -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -486,7 +486,7 @@
                             <button type="button"
                                 class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                 id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="sr-only">Open user menu</span>
+                                <span class="sr-only">{{ __('message.open_user_menu') }}</span>
                                 <img class="h-8 w-8 rounded-full"
                                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                                     alt="">
@@ -506,11 +506,11 @@
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <!-- Active: "bg-gray-100", Not Active: "" -->
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                                id="user-menu-item-0">Your Profile</a>
+                                id="user-menu-item-0">{{ __('message.your_profile') }}</a>
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                                id="user-menu-item-1">Settings</a>
+                                id="user-menu-item-1">{{ __('message.settings') }}</a>
                             <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
-                                id="user-menu-item-2">Sign out</a>
+                                id="user-menu-item-2">{{ __('message.sign_out') }}</a>
                         </div>
                     </div>
                 </div>
@@ -523,16 +523,16 @@
             <div class="space-y-1 px-2 pt-2 pb-3">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                    aria-current="page">Dashboard</a>
+                    aria-current="page">{{ __('message.home') }}</a>
 
                 <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">{{ __('message.movies') }}</a>
 
                 <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">{{ __('message.tv_series') }}</a>
 
                 <a href="#"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">{{ __('message.actres') }}</a>
             </div>
         </div>
     </nav>
