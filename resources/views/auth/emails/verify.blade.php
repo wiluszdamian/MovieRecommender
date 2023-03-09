99b8c9a25f1a7d86759a0f43d13278e917c1f126
wiluszdamian <damians.wilusz@gmail.com>
2023-03-09 20:40:09 +0100

    repo cleaning

--- resources/views/auth/emails/verify.blade.php
+++
@@ -1,6 +0,0 @@
-<p> {{ __('message.email_hello') }} {{ $user->name }},</p>
-<p> {{ __('message.email_thanks_for_register') }}</p>
-<a href="{{ route('verify', $user->token) }}"> {{ __('message.email_confirm_email') }}</a>
-<p> {{ __('message.email_if_not_register') }}</p>
-<p> {{ __('message.email_best_regards') }}</p>
-<p> {{ __('message.email_team') }}{{ config('app.name') }}</p>
